f91e2036a4a65232035c034707fdcd7a725cbb8a
cc231057 <146203052+cc231057@users.noreply.github.com>
2024-05-29 11:55:05 +0200

    About Page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,27 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+        <h1 className="text-2xl font-medium text-amber-600 ">Welcome to BingeWatch</h1>
+        <p>At BingeWatch, we're dedicated to transforming the way you experience movies.
+           Whether you're a casual viewer or a dedicated cinephile, our mission is to 
+           provide you with the ultimate movie-watching experience, all from the comfort 
+           of your own home.</p>
+        <h2 className="text-xl font-medium text-amber-500">Who We Are</h2>
+        <p>BingeWatch was founded by a group of movie enthusiasts who believe that watching 
+           films should be an immersive, enjoyable, and seamless experience. Our team is 
+           passionate about all things cinema, from the latest blockbusters to timeless 
+           classics. We strive to bring you a diverse selection of movies that cater to every 
+           taste and mood.</p>
+        <h2 className="text-xl font-medium text-amber-500">Our Mission</h2>
+        <p>Our mission at BingeWatch is to create a community of movie lovers who can share 
+           their passion for cinema. We aim to foster a platform where you can explore,
+           discuss, and celebrate movies. We believe in the power of films to entertain, 
+           inspire, and bring people together, and we're committed to delivering that 
+           experience to you.</p>
+        <h2 className="text-xl font-medium text-amber-500">Join Us</h2>
+        <p>Join the BingeWatch community today and dive into a world of endless entertainment.
+           Whether you're planning a movie night with friends, a cozy evening alone, or a family 
+           movie marathon, BingeWatch has got you covered.</p>
+    </div>
+  )
+}
